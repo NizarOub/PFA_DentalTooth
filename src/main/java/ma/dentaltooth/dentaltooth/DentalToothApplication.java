65756b8a5290f872ac8e3255d37a6054b0dd16b4
Nizar Oubbali <nizar.oubbali@gmail.com>
2023-05-01 23:28:40 +0100

    first

---
+++ src/main/java/ma/dentaltooth/dentaltooth/DentalToothApplication.java
@@ -0,0 +1,13 @@
+package ma.dentaltooth.dentaltooth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalToothApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentalToothApplication.class, args);
+    }
+
+}
